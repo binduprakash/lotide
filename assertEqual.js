ba6cd9e6f5d9960e49a9f349baa2bb2591f7507c
Bindu Prakash <bindu2305@gmail.com>
2019-11-13 04:09:15 +0000

    assert equal done

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+    if(actual === expected){
+        console.log("Assertion Passed")
+    } 
+    else if(actual !== expected){
+        console.log("Assertion Failed")
+    }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
